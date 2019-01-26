1d67511e64f54ac08bfd878afff44342ac0df629
Owner <Owner@10.30.18.154>
2019-01-26 11:15:32 -0600

    set up fnctns

--- prepare_lidar_dataset.py
+++ prepare_lidar_dataset.py
@@ -1,8 +1,30 @@
+import numpy as np
+import h5py
+
 """ This module:
         (1) takes in compressed raw lidar data
         (2) identifies vehicles in the compressed data
-        (3) processes data for each vehicle
-        (4) stores this processed data as m x n images in hfd5 file
+        (3) processes and normalizes data for each vehicle
+        (4) stores processed vehicle data as m x n images in hfd5 file
         
     This module works for both 1D and 2D (scanning) lidar data
 """
+
+def read_in_data():
+    """ reads in data from file, returns a numpy array of data """
+    pass
+
+def process_data():
+    """ takes in numpy array of data, processes it, returns numpy array of data """
+    pass
+
+def store_as_hdf5():
+    """ stores the numpy array as hdf5 file """
+    pass
+
+def detect_vehicles():
+    """ returns an array of starting and ending indeces of vehicles, given a numpy array of raw data """
+    pass
+
+def main():
+    pass
